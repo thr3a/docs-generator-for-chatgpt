11112b9a2f0dda00c7bfd2a5802ebda4e14ec708
thr3a <thr3a@example.com>
2025-01-12 01:19:54 +0900

    add: uv

---
+++ uv/generate.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# リポジトリのURL
+REPO_URL="https://github.com/astral-sh/uv.git"
+# クローン先ディレクトリ名
+CLONE_DIR="uv"
+# 出力ファイル名
+OUTPUT_FILE="index.md"
+
+# クローンディレクトリの存在確認
+if [ ! -d "$CLONE_DIR" ]; then
+    # ディレクトリが存在しない場合、depth 1でクローン
+    git clone --depth 1 "$REPO_URL" "$CLONE_DIR"
+else
+    # ディレクトリが存在する場合、pull
+    cd "$CLONE_DIR"
+    git pull
+    cd ..
+fi
+
+# 必要なファイルを結合
+echo > $OUTPUT_FILE
+find "$CLONE_DIR/docs/" -name "*.md" -type f ! -name "index.md" | while read file; do
+    echo -e "\n# $(basename "$file")\n" >> $OUTPUT_FILE
+    cat "$file" | sed '1{/^---$/!q;};1,/^---$/d' >> $OUTPUT_FILE
+done
+docker run --rm -v ./:/app thr3a/remove-markdown-links index.md --override
+
+# クローンしたディレクトリを削除
+# rm -rf "$CLONE_DIR"
